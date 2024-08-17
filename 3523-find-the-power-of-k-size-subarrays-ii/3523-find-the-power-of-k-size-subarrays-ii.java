e456e47e4e88e7cb1d29205c978261888b3c08e0
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2024-08-17 22:07:06 +0530

    Time: 3 ms (100%), Space: 63.9 MB (50%) - LeetHub

--- 3523-find-the-power-of-k-size-subarrays-ii/3523-find-the-power-of-k-size-subarrays-ii.java
+++ 3523-find-the-power-of-k-size-subarrays-ii/3523-find-the-power-of-k-size-subarrays-ii.java
@@ -1,27 +1,21 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
-        int[] num = new int[nums.length];
-        int[] vec = new int[nums.length - k + 1];
-        num[0] = 1;
-        int a = 1;
-        // Create the prefix array num for all consecutive numbers
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i - 1] + 1 == nums[i]) {
-                a++;
-                num[i] = a;
-            } else {
-                a = 1;
-                num[i] = a;
+        int n = nums.length, cnt = 1;
+        if(k == 1) return nums;
+        int[] res = new int[n-k+1];
+        Arrays.fill(res, -1);
+
+        for(int i=1; i<n; i++)
+        {
+            if(nums[i] != nums[i-1]+1)
+            cnt = 1;
+            else
+            {
+                cnt++;
+                if(cnt >= k)
+                res[i-k+1] = nums[i];
             }
         }
-        // Find in the prefix vector if the difference is equal to k hence proving the sub array has consecutive number
-        for (int i = 0; i <= nums.length - k; i++) {
-            if ((num[i + k - 1] - num[i] + 1) == k) {
-                vec[i] = nums[i + k - 1];
-            } else {
-                vec[i] = -1;
-            }
-        }
-        return vec;
+        return res;
     }
 }
