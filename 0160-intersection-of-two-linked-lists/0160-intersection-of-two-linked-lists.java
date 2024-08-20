b882c9c80a0ef4384e12deda84445503ae42b193
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2024-08-20 11:24:39 +0530

    Time: 1 ms (99.89%), Space: 48.6 MB (32.21%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,23 +11,59 @@
  */
 public class Solution 
 {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) 
+    public ListNode getIntersectionNode(ListNode head1, ListNode head2) 
     {
-        HashSet <ListNode> list = new HashSet<>();
-        ListNode A = headA;
-        ListNode B = headB;
-        while(A != null )
+        ListNode temp1 = head1;
+        ListNode temp2 = head2;
+        int count1=0, count2=0;
+        ListNode ans = null;
+        while(temp1 != null)
         {
-            list.add(A);
-            A= A.next;
-
+            count1++;
+            temp1 = temp1.next;
         }
-        while(B != null) 
+        while(temp2 != null)
         {
-            if( list.contains(B))
-            return B;
-            B = B.next;
-        }       
+            count2++;
+            temp2 = temp2.next;
+        }
+        if(count1 > count2)
+        {
+            int loop1 = count1 - count2;
+            for(int i=1;i<= loop1;i++)
+            {
+            head1 = head1.next;
+            }
+            
+        }
+        if(count2 > count1)
+        {
+            int loop2 = count2 - count1;
+            for(int i=1;i<= loop2;i++)
+            {
+            head2 = head2.next;
+            }
+            
+        }
+        ans = comparison(head1, head2);
+        return ans;
+        
+        
+        // code here
+    }
+    ListNode comparison(ListNode head1, ListNode head2)
+    {
+        if(head1 == null || head2 == null)
+        return null;
+        while(head1 != null && head2 != null)
+        {
+            if(head1 == head2)
+            return head1;
+            
+            head1 = head1.next;
+            head2 = head2.next;
+            
+        }
         return null;
     }
 }
