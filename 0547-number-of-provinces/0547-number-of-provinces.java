114b07603c18195d7bdaabd0e297f05bb47a35f4
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2024-09-13 04:56:28 +0530

    Time: 1 ms (90.88%), Space: 47.9 MB (27.55%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,52 +1,23 @@
-class Solution 
-{
-    public int findCircleNum(int[][] isConnected) 
-    {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-        int V = isConnected.length;
-        for(int i = 0;i<V;i++) 
-        {
-            adj.add(new ArrayList<Integer>()); 
-        }
-        for(int i =0;i< isConnected.length;i++)
-        {
-            for(int j =0;j< isConnected[i].length;j++)
-            {
-                if(isConnected[i][j]==1 && i != j)
-                {
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-
-
+class Solution {
+    private void dfs(int node, int[][] adjmatrix, int v, boolean[] visited){
+        // perform dfs on one connected component
+        visited[node] = true;
+        for (int i = 0; i < v; i++){
+            if (adjmatrix[node][i] == 1 && !visited[i]){
+                dfs(i, adjmatrix, v, visited);
             }
         }
-
-        boolean vis[] = new boolean[V];
-        int count =0;
-        for(int i =0;i<V;i++)
-        {
-            if(vis[i]== false)
-            {
-                count ++;
-                dfs(adj,vis,i);
-            }
-
-        }
-        return count;
-        
-
-        
     }
-    public void dfs(ArrayList<ArrayList<Integer>> adj, boolean[] vis, int curr)
-    {
-        vis[curr] = true;
-        for(int i =0;i< adj.get(curr).size(); i++)
-        {
-            int e = adj.get(curr).get(i);
-            if(vis[e]== false)
-            dfs(adj,vis,e);
-
+    public int findCircleNum(int[][] adjmatrix) {
+        int v = adjmatrix.length;
+        boolean[] visited = new boolean[v];
+        int num_provinces = 0;
+        for (int i = 0; i < v; i++){
+            if (!visited[i]){
+                num_provinces++;
+                dfs(i, adjmatrix, v, visited);
+            }
         }
+        return num_provinces;
     }
 }
