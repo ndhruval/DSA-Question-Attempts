a385e94232befc277e3931ab3d14649109bdea1d
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2024-09-25 22:38:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest number on left/smallest-number-on-left.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int n = Integer.parseInt(in.readLine());
+            String a1[] = in.readLine().trim().split("\\s+");
+            int a[] = new int[n];
+            for(int i = 0;i < n;i++)
+                a[i] = Integer.parseInt(a1[i]);
+            Solution ob = new Solution();
+            List<Integer> ans = new ArrayList<Integer>();
+            ans = ob.leftSmaller(n, a);
+            for(int i = 0;i < n;i++)
+                System.out.print(ans.get(i)+" ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    static List<Integer> leftSmaller(int n, int a[])
+    {
+        List<Integer> list = new ArrayList<Integer>();
+        Deque<Integer> st = new ArrayDeque<>();
+
+        for(int i=0;i<n;i++)
+        {
+            if(st.isEmpty())
+                list.add(-1);
+            else if(st.peek()< a[i])
+                list.add(st.peek());
+            else
+            {
+                while(!st.isEmpty() && st.peek()>=a[i])
+                {
+                    st.pop();
+                }
+                if(st.isEmpty())
+                    list.add(-1);
+                else
+                    list.add(st.peek());
+            }
+            st.push(a[i]);
+        }
+        return list;
+
+    }
+}
