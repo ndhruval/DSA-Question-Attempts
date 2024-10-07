a8212d3c52290fa0bddf0e1f3b3580c3293de2a5
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2024-10-07 13:30:50 +0530

    Time: 1 ms (99.44%), Space: 57.4 MB (8.32%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum = 0;
+        int sum = nums[0];
         int max = nums[0];
 
         // for(int i = 0; i< nums.length; i++){
@@ -12,12 +12,11 @@ class Solution {
         //     }
         // }
 
-        for(int i=0; i<nums.length; i++){
-            sum += nums[i];
-            max= Math.max(sum, max);
-            if(sum < 0){
-                sum = 0;
-            }
+        for(int i=1; i<nums.length; i++)
+        {
+            sum = Math.max(nums[i], sum+ nums[i]);
+            max = Math.max(sum, max);
+            
         }
 
         return max;
