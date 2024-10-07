8fd8d09fe57d16aa8826ea879392b7b9ab57e969
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2024-10-07 12:46:28 +0530

    Time: 25 ms (49.88%), Space: 46.4 MB (44.21%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -11,14 +11,7 @@ class Solution {
             {
                  count+= map.get(sum-k);
             }
-            if(map.containsKey(sum))
-            {
-                map.put(sum,map.get(sum)+1);
-            }
-            else
-            {
-                map.put(sum,1);
-            }
+            map.put(sum, map.getOrDefault(sum,0)+1);
         }
         return count;
     }
