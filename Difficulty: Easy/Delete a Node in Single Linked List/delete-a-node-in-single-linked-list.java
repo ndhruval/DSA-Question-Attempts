f2d671338fabcb433dddb672ecc7edfbaa557f4c
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2024-10-14 07:07:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete a Node in Single Linked List/delete-a-node-in-single-linked-list.java
+++ Difficulty: Easy/Delete a Node in Single Linked List/delete-a-node-in-single-linked-list.java
@@ -1,113 +1,104 @@
 //{ Driver Code Starts
-import java.util.*;
-class Node
-{
-	int data;
-	Node next;
-	
-	Node(int d)
-	{
-		data = d;
-		next = null;
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
 }
-class DeleteNode
-{
-	Node head;
-	void printList(Node head)
-    {
+
+class DeleteNode {
+    Node head;
+
+    void printList(Node head) {
         Node temp = head;
-        while (temp != null)
-        {
-           System.out.print(temp.data+" ");
-           temp = temp.next;
-        }  
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
         System.out.println();
     }
-	public void addToTheLast(Node node) 
-	{
-		if (head == null) 
-		{
-			head = node;
-		} else 
-		{
-		   Node temp = head;
-		   while (temp.next != null)
-		   temp = temp.next;
 
-		   temp.next = node;
-		}}
-	public static void main(String args[])
-    {
-       
-         Scanner sc = new Scanner(System.in);
-		 int t  =sc.nextInt();
-		 
-		 while(t>0)
-         {
-			int n = sc.nextInt();
-			//int k = sc.nextInt();
-			DeleteNode llist = new DeleteNode();
-			//int n=Integer.parseInt(br.readLine());
-			int a1 = sc.nextInt();
-			Node head = new Node(a1);
-            llist.addToTheLast(head);
-            for (int i = 1; i < n; i++) 
-			{
-				int a = sc.nextInt(); 
-				llist.addToTheLast(new Node(a));
-			}
-			int x = sc.nextInt();
-			//System.out.println(llist.head.data);
-			  GfG g = new GfG(); 
-			//System.out.println(k);
-			//System.out.println(g.getNthFromLast(llist.head,k));
-			Node result = g.deleteNode(llist.head, x);
-			llist.printList(result);
-		t--;
-		}
-	}
+    public void addToTheLast(Node node) {
+        if (head == null) {
+            head = node;
+        } else {
+            Node temp = head;
+            while (temp.next != null) temp = temp.next;
+            temp.next = node;
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            String[] s = in.readLine().trim().split(" ");
+            Node head = new Node(Integer.parseInt(s[0]));
+            Node copy = head;
+            for (int i = 1; i < s.length; i++) {
+                Node temp = new Node(Integer.parseInt(s[i]));
+                copy.next = temp;
+                copy = copy.next;
+            }
+
+            int x = Integer.parseInt(in.readLine());
+
+            Solution ob = new Solution();
+            Node ans = ob.deleteNode(head, x);
+
+            while (ans != null) {
+                System.out.print(ans.data + " ");
+                ans = ans.next;
+            }
+            System.out.println();
+
+            t--;
+        }
+    }
 }
+
 // } Driver Code Ends
 
 
 /* Linklist node structure
 class Node
 {
-	int data;
-	Node next;
-	
-	Node(int d)
-	{
-		data = d;
-		next = null;
-	}
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
 }*/
-/*You are required to complete below method*/
-class GfG
+class Solution 
 {
-    Node deleteNode(Node head, int x)
+    Node deleteNode(Node head, int x) 
     {
         if (x == 1) 
         {
-            return head.next;  // Simply move the head to the next node
+            return head.next;
         }
-
-        
-        Node temp = head;
-        for (int i = 1; temp != null && i < x - 1; i++) {
-            temp = temp.next;
+        Node dum = head;
+        int t = 1;
+        while(dum != null && t< x-1)
+        {
+            dum = dum.next;
+            t++;
         }
-
-        // If `temp` is null or the next node is null, `x` is out of bounds
-        if (temp == null || temp.next == null) {
-            return head;  // Return the original list since no deletion is possible
+        if (dum != null && dum.next != null) 
+        {
+            dum.next =dum.next.next;
         }
-
-        // Delete the node by skipping over it
-        temp.next = temp.next.next;
-
         return head;
-	// Your code here	
+        // code here
     }
 }
