7ef8f41c50276a02128dcc7804215076c89ca9a5
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2024-12-08 12:14:58 +0530

    Time: 0 ms (100%), Space: 40.8 MB (9.29%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,16 +1,20 @@
-class Solution {
-    public int fib(int n) {
-        if(n<=1) return n;
+class Solution 
+{
+    public int fib(int n) 
+    {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        int y = fibo(n,dp);
+        return y;
+
+
         
-        int first=0,second=1;
-
-        while(n>1) {
-            int sum=first+second;
-            first=second;
-            second=sum;
-            n--;
-        }
-
-        return second;
+    }
+    public static int fibo(int n, int[] dp)
+    {
+        if(n<=1)
+        return n;
+        if(dp[n]!= -1) return dp[n];
+        return dp[n] = fibo(n-1,dp) + fibo(n-2,dp);
     }
 }
