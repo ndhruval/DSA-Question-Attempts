00a5bfa52eb6f91390a26e50a66b447b630074a2
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-01-29 22:53:49 +0530

    Time: 3 ms (14.72%), Space: 42.4 MB (74.81%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,61 +1,88 @@
-import java.util.*;
-class element{
-    int row,col,min;
-    public element( int r, int c, int m)
+class Pair{
+    int row;
+    int col;
+    int time;
+
+    Pair(int r, int c, int t)
     {
         this.row = r;
         this.col = c;
-        this.min = m;
+        this.time = t;
     }
 }
 
-class Solution 
-{
-    public int orangesRotting(int[][] grid) 
-    {
-        int r = grid.length;
-        int c = grid[0].length;
-        int fresh =0;
-        Queue<element> q = new ArrayDeque<>();
-        for(int i =0;i<r;i++)
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+        int visited[][] = new int[n][m];
+        Queue<Pair> q = new LinkedList<>();
+        int total =0;
+
+        for(int i =0;i<n;i++)
         {
-            for(int j=0;j<c;j++)
+            for(int j =0;j<m;j++)
             {
-                if(grid[i][j]==1)
-                fresh++;
-                else if(grid[i][j]==2)
+                if(grid[i][j] == 2)
                 {
-                    q.offer(new element(i,j,0));
-                    grid[i][j]=0;
+                visited[i][j]= 2;
+                q.add(new Pair(i,j,0));
                 }
+                else
+                visited[i][j]=0;
+
+                if(grid[i][j] != 0)
+                total++;
+
             }
         }
-        if(q.isEmpty() && fresh == 0)
-        return 0;
 
-        int maxMins =0;
-        int rowd[] = {-1,0,0,1};
-        int cold[] = {0,-1,1,0};
+        int tm =0;
+        int drow[] = {-1,0,+1,0};
+        int dcol[] = {0,+1,0,-1};
+        
 
         while(!q.isEmpty())
         {
-            element curr = q.poll();
-            maxMins = Math.max(maxMins, curr.min);
+            int r = q.peek().row;
+            int c = q.peek().col;
+            int t = q.peek().time;
+            q.remove();
+            tm = Math.max(tm,t);
             for(int i=0;i<4;i++)
             {
-                int adjr = curr.row + rowd[i];
-                int adjc = curr.col + cold[i];
-                if(adjr >=0 && adjr< r && adjc>=0 && adjc<c && grid[adjr][adjc]==1)
+                int nrow = r +drow[i];
+                int ncol = c +dcol[i];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]==1 && visited[nrow][ncol]==0 )
                 {
-                    q.add(new element(adjr, adjc, curr.min +1));
-                    fresh --;
-                    grid[adjr][adjc] =0;
+                    visited[nrow][ncol]=2;
+                    q.add(new Pair(nrow, ncol, t+1));
                 }
             }
+
+            
+
         }
-        if(fresh > 0)
-        return -1;
-        return maxMins;
+        int count=0;
+            for(int i=0;i<n;i++)
+            {
+                for(int j =0;j<m;j++)
+                {
+                    if(visited[i][j]==2)
+                    count ++;
+                }
+            }
+            if(count == total)
+            return tm;
+            else
+            return -1;
+
+
+
+       
+
         
     }
 }
