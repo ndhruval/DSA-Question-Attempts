bf069be6bef08ceee6ee4ba2c07551637d048dcc
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-03-02 11:12:51 +0530

    Time: 3 ms (99.03%), Space: 45.8 MB (18.84%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean doesAliceWin(String s) {
         int n= s.length();
-        int vow =0;
+       
         for(int i=0;i<n;i++)
         {
             char c = s.charAt(i);
             if(c =='a'||c=='e'|| c== 'i'|| c== 'o' ||c=='u')
-            vow++;
+            return true;
 
         }
-        if(vow ==0)
         return false;
-        else 
-        return true;
+        
         
     }
 }
