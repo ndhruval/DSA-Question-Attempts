c4103b988779ba016d7ca03259a110916f1f2d4a
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-03-18 07:10:16 +0530

    Time: 3 ms (98.64%), Space: 56.2 MB (12.81%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.java
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.java
@@ -1,43 +1,20 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) {
-        int ans =1;
-        int l =0, h = nums.length;
-        while(l<=h)
+        int usedBits = 0;
+        int start =0;
+        int max = 0;
+
+        for(int end =0;end<nums.length; ++end)
         {
-            int mid = l+ (h-l)/2;
-            if(canformnice(mid,nums))
+            while((usedBits & nums[end])!=0)
             {
-                ans = mid;
-                l = mid+1;
+                usedBits ^= nums[start];
+                start++;
             }
-            else
-            h = mid-1;
+            usedBits |= nums[end];
+            max = Math.max(max, end - start +1);
         }
-        return ans;
+        return max;
         
-    }
-    public static boolean canformnice(int length, int[] nums)
-    {
-        if(length<=1)
-        return true;
-
-        for(int start =0;start<= nums.length - length; ++start)
-        {
-            int bitmask = 0;
-            boolean isNice = true;
-
-            for(int pos = start;pos< start+length;++pos)
-            {
-                if((bitmask & nums[pos])!=0)
-                {
-                    isNice = false;
-                    break;
-                }
-                bitmask |= nums[pos];
-            }
-            if(isNice)
-            return true;
-        }
-        return false;
     }
 }
