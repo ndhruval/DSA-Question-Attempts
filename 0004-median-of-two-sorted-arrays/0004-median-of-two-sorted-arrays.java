7856e12c29602b6af2c762fd71b04f5cd77a0402
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-03-21 12:14:33 +0530

    Time: 1 ms (100%), Space: 45.9 MB (84.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,26 +1,34 @@
-import java.util.*;
-class Solution
- {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) 
-    {
-        int n= nums1.length + nums2.length;
-        int a[] = new int[n];int k=0,j=0,i=0;
-        double median;
-        while(i<nums1.length)
-        {
-            a[k++]= nums1[i++];
-        }
-        while(j< nums2.length)
-        a[k++]= nums2[j++];
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums1.length > nums2.length)
+        return findMedianSortedArrays(nums2, nums1);
 
-        Arrays.sort(a);
-        if(n%2==0)
+        int left = (nums1.length + nums2.length +1)/2;
+        int l = 0;
+        int h = nums1.length;
+
+        while(l<= h)
         {
-            median= (double)(a[n/2] + a[n/2-1])/2;
-            
+            int mid1 = (l+h)/2;
+            int mid2 = left - mid1;
+
+            int l1 = (mid1>0)?nums1[mid1-1]: Integer.MIN_VALUE;
+            int l2 = (mid2>0)?nums2[mid2-1]:Integer.MIN_VALUE;
+            int r1 = (mid1<nums1.length)?nums1[mid1]:Integer.MAX_VALUE;
+            int r2 = (mid2<nums2.length)?nums2[mid2]:Integer.MAX_VALUE;
+
+            if(l1<=r2 && l2<=r1)
+            {
+                if((nums1.length+ nums2.length)%2 == 1)
+                return Math.max(l1,l2);
+                else
+                return (Math.max(l1,l2)+ Math.min(r1,r2))/2.0;
+            }
+            else if(l1>r2)
+            h=mid1-1;
+            else
+            l = mid1+1;
         }
-        else
-        median = a[(int)n/2];
-        return median;
+        return 0;
     }
 }
