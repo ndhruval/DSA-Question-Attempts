93efa7664b2c82ca0c1109df1362a7eeb4c6ef88
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-05-06 13:37:59 +0530

    Time: 1 ms (77.9%), Space: 45.8 MB (70.89%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int sum=0;
+        int maxPro = Integer.MIN_VALUE;
+        for(int i=1;i<n;i++)
+        {
+            if(prices[i-1]<prices[i]){
+                sum += prices[i]- prices[i-1];
+            }
+
+        }
+        return sum;
+
+        
+    }
+}
