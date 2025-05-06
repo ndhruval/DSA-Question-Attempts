cacc3182eec9e981d5cdb9d453a6a00b2c840153
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-05-06 10:41:32 +0530

    Time: 2 ms (13.24%), Space: 42 MB (65.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,34 +1,20 @@
-class Solution 
-{
-    public void sortColors(int[] nums) 
-    {
-        int cnt[] = new int[3];
-        for(int i =0;i< nums.length;i++)
+class Solution {
+    public void sortColors(int[] nums) {
+        int n= nums.length;
+
+        for(int i=0;i<n-1;i++)
         {
-            if(nums[i]==0)
-            cnt[0]++;
-            else if(nums[i]==1)
-            cnt[1]++;
-            else
-            cnt[2]++;
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(nums[j]>nums[j+1])
+                {
+                    int temp = nums[j];
+                    nums[j]= nums[j+1];
+                    nums[j+1]= temp;
+                }
+            }
         }
-        int x=0;
-        for(int i=0;i<cnt[0];i++)
-        {
-            nums[x]=0;
-            x++;
-        }
-        for(int i=0;i<cnt[1];i++)
-        {
-            nums[x] = 1;
-            x++;
-        }
-        for(int i=0; i<cnt[2];i++)
-        {
-            nums[x] =2;
-            x++;
-        }
-       
+
         
     }
 }
