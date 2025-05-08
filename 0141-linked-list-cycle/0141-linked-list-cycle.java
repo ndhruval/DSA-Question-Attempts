2cb2662edbbd3a86bb91fe892d0a340f2d0a32e6
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-05-08 11:35:37 +0530

    Time: 0 ms (100%), Space: 44.4 MB (76.74%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,22 +9,23 @@
  *     }
  * }
  */
-public class Solution 
-{
-    public boolean hasCycle(ListNode head) 
-{
-   if (head == null) return false;
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null)
+        return false;
 
-   ListNode slow = head;
-   ListNode fast = head;
-   while(fast != null && fast.next != null)
-   {
-       slow = slow.next;
-       fast = fast.next.next;
-       if(slow == fast)
-           return true;
-   }
-   return false;
-}
+        ListNode fast = head;
+        ListNode slow = head;
 
+        while(fast!= null && fast.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow)
+            return true;
+        }
+        return false;
+        
+    }
 }
