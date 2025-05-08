63d57052a318b5ccfdf1393e228821a5e607de0a
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-05-08 10:54:49 +0530

    Time: 17 ms (7.49%), Space: 45.6 MB (7.1%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,37 +1,27 @@
 class Solution {
     public String reverseWords(String s) {
+        s = s.trim();
+        Stack<String> st = new Stack<>();
         String temp = "";
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != ' ') {
+                temp += c;
+            } else if (temp.length() > 0) { // to skip multiple spaces
+                st.push(temp);
+                temp = "";
+            }
+        }
+        st.push(temp); // push the last word
+
         String ans = "";
-        s = s.trim();  // Remove leading and trailing spaces
-
-        int l = s.length();
-
-        for (int i = l - 1; i >= 0; i--) {
-            char ch = s.charAt(i);
-            if (ch != ' ') {
-                temp = ch + temp;
-            } else {
-                // Add word to the answer only if temp is not empty
-                if (!temp.equals("")) {
-                    if (!ans.equals("")) {
-                        ans = ans + " " + temp;
-                    } else {
-                        ans = temp;
-                    }
-                    temp = "";  // Reset temp for the next word
-                }
+        while (!st.isEmpty()) {
+            ans += st.pop();
+            if (!st.isEmpty()) {
+                ans += " ";
             }
         }
-
-        // Add the last word if there's any left in temp
-        if (!temp.equals("")) {
-            if (!ans.equals("")) {
-                ans = ans + " " + temp;
-            } else {
-                ans = temp;
-            }
-        }
-
         return ans;
     }
 }
