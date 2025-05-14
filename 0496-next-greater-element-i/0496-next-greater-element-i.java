1beb6e5f97aa03b1bc30f61de470f06424093de9
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-05-14 18:46:04 +0530

    Time: 5 ms (17.43%), Space: 43.3 MB (81.2%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,31 +1,39 @@
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2)
-    {
-        int[] res = new int[nums1.length];
-        Arrays.fill(res, -1);
-        HashMap<Integer,Integer> map = new HashMap<>();
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-        for(int i=0;i< nums2.length ;i++)
-        {
-            map.put(nums2[i], i);
+        Stack<Integer> st = new Stack<>();
+        int[] nge = new int[nums2.length]; // Next Greater Element array
+
+        // Step 1: Compute NGE for each element in nums2
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            // Pop all smaller or equal elements from stack
+            while (!st.isEmpty() && st.peek() <= nums2[i]) {
+                st.pop();
+            }
+
+            // If stack is empty, no greater element to the right
+            if (st.isEmpty()) {
+                nge[i] = -1;
+            } else {
+                nge[i] = st.peek();
+            }
+
+            // Push current element to stack
+            st.push(nums2[i]);
         }
-        for(int i =0;i< nums1.length;i++)
-        {
-            if(map.containsKey(nums1[i]))
-            {
-                int t = map.get(nums1[i]);
-                for(int j = t+1 ; j< nums2.length;j++)
-                {
-                    if(nums1[i] < nums2[j])
-                    {
-                        res[i] = nums2[j];
-                        break;
-                    }
-                    
+
+        // Step 2: Find result for elements in nums1 by looking up in nums2
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    res[i] = nge[j]; // Use precomputed NGE
+                    break;
                 }
             }
         }
-        return res;
 
+        return res;
     }
 }
+
