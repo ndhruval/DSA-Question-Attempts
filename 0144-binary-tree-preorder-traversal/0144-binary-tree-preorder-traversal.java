2e61ce1994dcace23bd9d094c73a2a5265f4d361
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-05-21 10:33:23 +0530

    Time: 0 ms (100%), Space: 41.4 MB (92.24%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -13,23 +13,20 @@
  *     }
  * }
  */
-class Solution 
-{
-    public List<Integer> preorderTraversal(TreeNode root) 
-    {
-        List<Integer> list = new ArrayList<Integer>();
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
         helper(root,list);
-        return list;
 
+        return list;
 
         
     }
-    public static void helper(TreeNode root, List<Integer> list)
-    {
+    public void helper(TreeNode root, List<Integer> list){
         if(root == null)
         return;
-
         list.add(root.val);
+
         helper(root.left,list);
         helper(root.right,list);
     }
