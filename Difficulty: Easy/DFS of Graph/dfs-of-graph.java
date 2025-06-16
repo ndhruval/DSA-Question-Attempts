46a728f9710c7d27c7e4cfaf2fd7778c339afa4d
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-06-16 12:12:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.java
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -1,61 +1,21 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
-            for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.dfsOfGraph(adj);
-            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     // Function to return a list containing the DFS traversal of the graph.
-    public ArrayList<Integer> dfsOfGraph(ArrayList<ArrayList<Integer>> adj) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> ans = new ArrayList<>();
         int V = adj.size();
-        boolean[] visited = new boolean[V+1];
-        visited[0] = true;
-        dfs(0,result,adj,visited);
-        return result;
+        boolean vis[] = new boolean[V];
+        DepthFirst(0,adj,vis,ans);
+        return ans;
         
         // Code here
-    }
-    public static void dfs(int node, ArrayList<Integer> result, ArrayList<ArrayList<Integer>> adj, boolean[] visited)
-    {
-        visited[node]= true;
-        result.add(node);
-        for(Integer it : adj.get(node))
-        {
-            if(!visited[it])
-            dfs(it, result,adj,visited);
-        }
         
     }
-    
+    public void DepthFirst(int node, ArrayList<ArrayList<Integer>> adj, boolean vis[], ArrayList<Integer> ans){
+        vis[node] = true;
+        ans.add(node);
+        for(Integer it: adj.get(node)){
+            if(!vis[it])
+            DepthFirst(it,adj, vis, ans);
+        }
+    }
 }
