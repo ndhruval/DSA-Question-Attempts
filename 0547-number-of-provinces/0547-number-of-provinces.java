dd414d2015c05466f8b2a4e4401ea6cb90051165
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-06-16 12:17:54 +0530

    Time: 3 ms (33.54%), Space: 47.5 MB (69.49%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,23 +1,57 @@
 class Solution {
-    private void dfs(int node, int[][] adjmatrix, int v, boolean[] visited){
-        // perform dfs on one connected component
-        visited[node] = true;
-        for (int i = 0; i < v; i++){
-            if (adjmatrix[node][i] == 1 && !visited[i]){
-                dfs(i, adjmatrix, v, visited);
+    public int findCircleNum(int[][] isConnected) 
+    {
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int n = isConnected.length;
+        for(int i=0;i<n;i++)
+        {
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(isConnected[i][j] == 1 && i!= j)
+                {
+                    adj.get(i).add(j);
+                }
             }
         }
+
+        boolean visited[] = new boolean[n];
+        int count =0;
+        for(int i=0;i<n;i++)
+        {
+            if(!visited[i])
+            {
+                count ++;
+                bfs(adj, visited,i);
+            }
+        }
+        return count;
+
+
+        
     }
-    public int findCircleNum(int[][] adjmatrix) {
-        int v = adjmatrix.length;
-        boolean[] visited = new boolean[v];
-        int num_provinces = 0;
-        for (int i = 0; i < v; i++){
-            if (!visited[i]){
-                num_provinces++;
-                dfs(i, adjmatrix, v, visited);
+
+    public static void bfs(ArrayList<ArrayList<Integer>> adj, boolean[] visited, int start)
+    {
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.offer(start);
+        visited[start]= true;
+        while(!q.isEmpty())
+        {
+            Integer node = q.poll();
+            for(Integer it : adj.get(node))
+            {
+                if(visited[it]== false)
+                {
+                    visited[it] = true;
+                    q.add(it);
+                }
             }
         }
-        return num_provinces;
+        return;
     }
 }
