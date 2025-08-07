b7193e9f10e19c8e91f8a80b8bfe444a48983ca8
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-08-08 01:15:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
@@ -1,24 +1,23 @@
 class Solution {
     static int matrixMultiplication(int arr[]) {
-        int dp[][] = new int[arr.length+1][arr.length+1];
-        for(int i=0;i<= arr.length;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        
-        int ans= solve(arr,1,arr.length-1,dp);
-        return ans;
+        int n = arr.length;
+        int min = Integer.MAX_VALUE;
+        int dp[][] = new int[n][n];
+        for(int i=0;i<n;i++) Arrays.fill(dp[i], -1);
+        return function(arr,1,n-1,min, dp);
+        // code here
         
     }
-    static int solve(int arr[], int i, int j, int dp[][]){
-        if(i>=j)
-        return 0;
-        if(dp[i][j] != -1)
-        return dp[i][j];
-        int min = Integer.MAX_VALUE;
-        for(int k =i;k<j;k++){
-            int temp = solve(arr,i,k,dp)+ solve(arr,k+1,j,dp)+ (arr[i-1]*arr[k]*arr[j]);
-            min = Math.min(min, temp);
+    public static int function(int arr[], int i, int j, int min, int[][] dp){
+        if(i == j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        for(int k =i;k< j;k++){
+            int ans = function(arr,i ,k,min, dp) + function(arr, k+1, j, min, dp
+            ) 
+            + arr[i-1]*arr[k]*arr[j];
+            min = Math.min(ans, min);
         }
-        return dp[i][j]=min;
+        return dp[i][j] = min;
+        
     }
 }
