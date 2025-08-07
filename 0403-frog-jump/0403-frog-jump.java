7c0b5731f52020077e10fd7acf676676170fc998
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-08-07 19:11:54 +0530

    Time: 4 ms (99.8%), Space: 46.6 MB (92.44%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,39 +1,31 @@
 class Solution {
     public boolean canCross(int[] stones) {
-        // A map for O(1) lookup of a stone's index given its position.
-        HashMap<Integer, Integer> stoneMap = new HashMap<>();
-        for (int i = 0; i < stones.length; i++) {
-            stoneMap.put(stones[i], i);
-        }
 
-        Boolean[][] memo = new Boolean[stones.length][stones.length];
-        
-        return solve(0, 0, stones, stoneMap, memo);
+        if(stones[1]-stones[0] != 1) return false;
+        boolean dp[][] = new boolean[stones.length][stones.length];
+        return function(0,1,stones,dp);
+
+                
     }
+    public static boolean function(int prev, int curr, int[] stones, boolean[][] dp){
+        if(curr == stones.length -1) return true;
+        if(dp[prev][curr] == true) return false;
+        
+        int lastjump = stones[curr] - stones[prev];
+        int next = curr +1;
+        while(next < stones.length && stones[next]<= stones[curr]+ lastjump + 1){
+            int nextJump = stones[next] - stones[curr];
 
-    private boolean solve(int index, int lastJump, int[] stones, HashMap<Integer, Integer> stoneMap, Boolean[][] memo) {
-        if (index == stones.length - 1) {
-            return true;
-        }
-
-        if (memo[index][lastJump] != null) {
-            return memo[index][lastJump];
-        }
-
-        for (int nextJump = lastJump - 1; nextJump <= lastJump + 1; nextJump++) {
-            if (nextJump > 0) {
-                int nextPosition = stones[index] + nextJump;
-                if (stoneMap.containsKey(nextPosition)) {
-                    int nextIndex = stoneMap.get(nextPosition);
-                    if (solve(nextIndex, nextJump, stones, stoneMap, memo)) {
-                        memo[index][lastJump] = true;
-                        return true;
-                    }
-                }
+            int jump = nextJump - lastjump;
+            if(jump>=-1 && jump<=1)
+            {
+                if(function(curr, next, stones,dp)) return true;
             }
+            next++;
         }
-
-        memo[index][lastJump] = false;
+        dp[prev][curr] = true;
         return false;
+
+        
     }
 }
