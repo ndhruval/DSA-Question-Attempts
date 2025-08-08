e9e77ef0e851df90949a8fe85fc32fc8223a0426
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-08-08 11:50:53 +0530

    Time: 47 ms (52.73%), Space: 48 MB (47.31%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,38 +1,33 @@
-class Solution 
-{
-    public boolean canPartition(int[] nums) 
-    {
-        int sum=0;
-        for(int i = 0;i<nums.length;i++)
-        {
-            sum += nums[i];
-        }
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum =0;
+        int n = nums.length;
+        for(int i =0;i<n;i++)
+            {
+                sum += nums[i];
+            }
         if(sum%2 != 0)
-        return false;
-        sum = sum/2;
-        Boolean dp[][] = new Boolean[nums.length +1][sum+1];
+            return false;
 
-        for(int i =0;i<=nums.length;i++)
-        {
-            for(int j=0;j<= sum;j++)
-            {
-                if(i==0)
-                dp[i][j]= false;
-                if(j==0)
-                dp[i][j]= true;
-            }
+        sum = sum/2;
+        boolean dp[][] = new boolean[n+1][sum+1];
+
+        for(int j=0;j<=sum;j++){
+            dp[0][j] = false;
         }
-        for(int i =1;i<=nums.length;i++)
-        {
-            for(int j=1;j<= sum;j++)
-            {
+        for(int i =0;i<=n;i++){
+            dp[i][0] = true;
+        }
+
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=sum;j++){
                 if(nums[i-1]<=j)
-                dp[i][j]= dp[i-1][j- nums[i-1]] || dp[i-1][j];
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
                 else
-                dp[i][j]= dp[i-1][j];
+                    dp[i][j] = dp[i-1][j];
             }
         }
-        return dp[nums.length][sum];
+        return dp[n][sum];
         
     }
 }
