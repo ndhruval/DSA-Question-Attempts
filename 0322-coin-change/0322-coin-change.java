1489f48c56ffee68e0305f555032b9e75cbc1c94
NISTHA DHRUVAL <99194136+ndhruval@users.noreply.github.com>
2025-08-08 11:48:34 +0530

    Time: 16 ms (55.85%), Space: 44.9 MB (42.88%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,28 +1,30 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int dp[][] = new int[n + 1][amount + 1];
-
-        // Initialize dp array
-        for (int i = 0; i <= n; i++) dp[i][0] = 0; // 0 coins needed to make amount 0
-        for (int j = 1; j <= amount; j++) dp[0][j] = Integer.MAX_VALUE; // Impossible without coins
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (coins[i - 1] <= j) {
-                    // Include the coin only if the sub-problem is solvable
-                    if (dp[i][j - coins[i - 1]] != Integer.MAX_VALUE) {
-                        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - coins[i - 1]] + 1);
-                    } else {
-                        dp[i][j] = dp[i - 1][j];
-                    }
-                } else {
-                    dp[i][j] = dp[i - 1][j]; // Exclude the coin
-                }
-            }
+        Arrays.sort(coins);
+        int n=  coins.length;
+        int dp[][] = new int[n+1][amount+1];
+        for(int i =0;i<=n;i++){
+            dp[i][0] = 0; //0 coins are required to get the amount 0
         }
+        for(int j =0;j<= amount;j++){
+            dp[0][j] = Integer.MAX_VALUE; //INFINTE coins are needed with the value 0 to get the sums more the 0
+        }
+        for(int j =1;j<= amount;j++){
+            dp[1][j] =j%coins[0] == 0? j/coins[0] : Integer.MAX_VALUE -1;
+        }
+        for(int i =2;i<=n;i++){
+            for(int j= 1;j<= amount;j++){
+                if(coins[i-1]<=j){
+                    dp[i][j] = Math.min(dp[i][j-coins[i-1]]+1, dp[i-1][j]);
+                }
+                else
+                dp[i][j] = dp[i-1][j];
 
-        // Final result
-        return dp[n][amount] == Integer.MAX_VALUE ? -1 : dp[n][amount];
+            }
+            
+        }
+        if(dp[n][amount] == Integer.MAX_VALUE -1) return -1;
+        return dp[n][amount];
+        
     }
 }
